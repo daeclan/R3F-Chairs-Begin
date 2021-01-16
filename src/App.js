f2a5d7c6c78e706659b816b6ab2a70bf11012c61
Declan Murphy <declxn@gmail.com>
2021-01-16 16:31:04 -0600

    Got section component working, was offsetting in a weird way and had to debug slow.

--- src/App.js
+++ src/App.js
@@ -2,8 +2,25 @@ import React from "react";
 import "./App.scss";
 //Components
 import Header from "./components/header";
+import { Section } from "./components/section";
 import { Canvas } from "react-three-fiber";
-import { Html, RoundedBox } from 'drei'
+import { Html } from 'drei'
+
+const HTMLContent = () => {
+  return (
+    <Section factor={1.5} offset={0}>
+      <group position={[0, 0, 0]}>
+        <Html fullscreen>
+          <div className="container">
+            <h1 className="title">
+              Hello
+          </h1>
+          </div>
+        </Html>
+      </group>
+    </Section>
+  )
+}
 
 export default function App() {
   return (
@@ -12,21 +29,7 @@ export default function App() {
       <Canvas
         colorManagement
         camera={{ position: [0, 0, 10], fov: 70 }}>
-        {/* <Html fullscreen>
-          <div className="container">
-            <h1 className="title">
-              Hello
-            </h1>
-          </div>  
-        </Html> */}
-        <RoundedBox
-          args={[1, 1, 1]} // Width, Height and Depth of the box
-          radius={0.05} // Border-Radius of the box
-          smoothness={4} // Optional, number of subdivisions
-        // {...meshProps} // All THREE.Mesh props are valid
-        >
-          <meshPhongMaterial attach="material" color="#f3f3f3" />
-        </RoundedBox>
+        <HTMLContent />
       </Canvas>
     </>
   );
