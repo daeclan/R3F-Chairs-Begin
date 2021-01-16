e2e47688469494b1cec0b8776eb9e55980133549
Declan Murphy <declxn@gmail.com>
2021-01-16 17:28:47 -0600

    Figured out group logic, passing in props to have a more dynamic code.

--- src/App.js
+++ src/App.js
@@ -6,8 +6,8 @@ import { Section } from "./components/section";
 import { Canvas, useFrame } from "react-three-fiber";
 import { Html, useGLTFLoader } from 'drei'
 
-const Model = () => {
-  const gltf = useGLTFLoader('/armchairYellow.gltf', true)
+const Model = ({ modelPath }) => {
+  const gltf = useGLTFLoader(modelPath, true)
   return <primitive object={gltf.scene} dispose={null} />
 }
 
@@ -22,22 +22,21 @@ const Lights = () => {
   )
 }
 
-const HTMLContent = () => {
+const HTMLContent = ({ children, modelPath, positionY }) => {
 
   const ref = useRef()
   useFrame(() => (ref.current.rotation.y += 0.01))
 
   return (
     <Section factor={1.5} offset={0}>
-      <group position={[0, 0, 0]}>
+      <group position={[0, positionY, 0]}>
+        {console.log(positionY)}
         <mesh ref={ref} position={[0, -3.14, 0]} scale={[0.1, 0.1, 0.1]}>
-          <Model />
+          <Model modelPath={modelPath} />
         </mesh>
         <Html fullscreen>
           <div className="container">
-            <h1 className="title">
-              Hello
-          </h1>
+            {children}
           </div>
         </Html>
       </group>
@@ -52,7 +51,27 @@ export default function App() {
       <Canvas colorManagement camera={{ position: [0, 0, 10], fov: 70 }}>
         <Lights />
         <Suspense fallback={null}>
-          <HTMLContent />
+          <HTMLContent modelPath="/armchairYellow.gltf" positionY={0}>
+            <div className="container">
+              <h1 className="title">
+                Hi
+            </h1>
+            </div>
+          </HTMLContent>
+          <HTMLContent modelPath="/armchairGreen.gltf" positionY={-8}>
+            <div className="container">
+              <h1 className="title">
+                Why Hello
+            </h1>
+            </div>
+          </HTMLContent>
+          <HTMLContent modelPath="/armchairGray.gltf" positionY={-16}>
+            <div className="container">
+              <h1 className="title">
+                Hi dare
+            </h1>
+            </div>
+          </HTMLContent>
         </Suspense>
       </Canvas>
     </>
